5af70d22016733853862b390c91f40c99c68f6b5
Sherry90 <hhlsdkm@gmail.com>
2024-05-13 17:02:40 +0900

    feat: route meals page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
+        <div>
+          <Link href={"/meals"}>meals</Link>
+        </div>
       </h1>
     </main>
   );
