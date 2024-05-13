0dfd0e58eb6cb9415d6a821f4b13bd5e74400b5e
Sherry90 <hhlsdkm@gmail.com>
2024-05-13 22:39:26 +0900

    fix: <div> -> <p>

--- app/page.js
+++ app/page.js
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Home () {
   return (
     <main>
       <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
-        <div>
-          <Link href={"/meals"}>meals</Link>
-        </div>
-        <div>
-          <Link href={"/meals/share"}>meals share</Link>
-        </div>
-        <div>
-          <Link href={"/community"}>community</Link>
-        </div>
+        <p>
+          <Link href={"/meals"}>Meals</Link>
+        </p>
+        <p>
+          <Link href={"/meals/share"}>Share Meal</Link>
+        </p>
+        <p>
+          <Link href={"/community"}>Community</Link>
+        </p>
       </h1>
     </main>
   );
