3f769efdde77acbb0af9437a4ff7c0299758a80e
Sherry90 <hhlsdkm@gmail.com>
2024-05-13 17:26:51 +0900

    feag: meals/[slug]/ dynamic route

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 const MealsPage = () => {
-  return <p>Meals Page</p>
-}
+  return (
+    <main>
+      <h1>Meals Page</h1>
+      <p>
+        <Link href={"/meals/food-1"}>Food 1</Link>
+      </p>
+      <p>
+        <Link href={"/meals/food-2"}>Food 2</Link>
+      </p>
+    </main>
+  );
+};
 
 export default MealsPage;
