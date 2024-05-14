0ba9780eb376b9bd6b291609b70fd3c864da9da5
Sherry90 <hhlsdkm@gmail.com>
2024-05-14 17:44:40 +0900

    feat: MainHeader nav Link router

--- components/main-header.js
+++ components/main-header.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
+import logoImg from "@/assets/logo.png"
+
 const MainHeader = () => {
-  return <header></header>
+  return <header>
+    <Link href={"/"}>
+      <img src={logoImg.src} alt="food"/>
+      NextLevel Food
+    </Link>
+
+    <nav>
+      <ul>
+        <li>
+          <Link href={"/meals"}>Browse Meals</Link>
+        </li>
+        <li>
+          <Link href={"/community"}>Foodies Community</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 }
 
 export default MainHeader;
