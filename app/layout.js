376f4b6a27dc779c4b16af310be5aba5bc4f946e
Sherry90 <hhlsdkm@gmail.com>
2024-05-14 17:44:50 +0900

    feat: MainHeader rendering

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import MainHeader from "@/components/main-header";
 
 export const metadata = {
   title: 'NextLevel Food',
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
           </svg>
         </div>
 
+        <MainHeader />
         {children}
       </body>
     </html>
