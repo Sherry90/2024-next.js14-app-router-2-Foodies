37321dd1352e6b8ae9876b026d36b9aad49b35eb
Sherry90 <hhlsdkm@gmail.com>
2024-05-15 01:28:58 +0900

    refactor: format

--- components/main-header.js
+++ components/main-header.js
@@ -1,27 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import logoImg from "@/assets/logo.png"
-import classes from "./main-header.module.css"
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
 
 const MainHeader = () => {
-  return <header className={classes.header}>
-    <Link className={classes.logo} href={"/"}>
-      <Image src={logoImg} alt="food" priority/>
-      NextLevel Food
-    </Link>
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href={"/"}>
+        <Image src={logoImg} alt="food" priority />
+        NextLevel Food
+      </Link>
 
-    <nav className={classes.nav}>
-      <ul>
-        <li>
-          <Link href={"/meals"}>Browse Meals</Link>
-        </li>
-        <li>
-          <Link href={"/community"}>Foodies Community</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-}
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href={"/meals"}>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href={"/community"}>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default MainHeader;
