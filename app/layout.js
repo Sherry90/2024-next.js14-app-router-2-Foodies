eea820fa205d5b2674aff563d43031e288a63cb7
Sherry90 <hhlsdkm@gmail.com>
2024-05-15 17:36:49 +0900

    refactor: format

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
-import './globals.css';
+import "./globals.css";
 import MainHeader from "@/components/main-header";
 
 export const metadata = {
-  title: 'NextLevel Food',
-  description: 'Delicious meals, shared by a food-loving community.',
+  title: "NextLevel Food",
+  description: "Delicious meals, shared by a food-loving community.",
 };
 
 export default function RootLayout({ children }) {
