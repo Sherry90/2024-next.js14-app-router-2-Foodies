843646343dce0d3d4191bfc4ac8d8d4adebae3d5
Sherry90 <hhlsdkm@gmail.com>
2024-05-15 17:44:18 +0900

    feat: import MainHeaderBackground component

--- components/main-header.js
+++ components/main-header.js
@@ -1,28 +1,32 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import MainHeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 
 const MainHeader = () => {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href={"/"}>
-        <Image src={logoImg} alt="food" priority />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href={"/"}>
+          <Image src={logoImg} alt="food" priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href={"/meals"}>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href={"/community"}>Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={"/community"}>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
