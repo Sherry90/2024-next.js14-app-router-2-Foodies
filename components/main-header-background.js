1074cbe809f354728150cf0a2b060c41cd1d4f07
Sherry90 <hhlsdkm@gmail.com>
2024-05-15 17:44:33 +0900

    feat: import css module

--- components/main-header-background.js
+++ components/main-header-background.js
@@ -1,6 +1,8 @@
+import classes from "./main-header-background.module.css";
+
 const MainHeaderBackground = () => {
   return (
-    <div className="header-background">
+    <div className={classes["header-background"]}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <defs>
           <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
