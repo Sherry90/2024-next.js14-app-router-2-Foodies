fd2c0e3405ca31e160aface4fb001e167d51085e
Sherry90 <hhlsdkm@gmail.com>
2024-05-16 17:03:17 +0900

    add: meals-grid

---
+++ components/meals/meals-grid.js
@@ -0,0 +1,13 @@
+import classes from "./meals-grid.module.css";
+
+const MealsGrid = ({ meals }) => {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}></li>
+      ))}
+    </ul>
+  );
+};
+
+export default MealsGrid;
