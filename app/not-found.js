703cca09de6ded7fba8425d9c2d863b847847624
Sherry90 <hhlsdkm@gmail.com>
2024-05-18 19:55:21 +0900

    feat: not found page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunate, we could not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFound;
