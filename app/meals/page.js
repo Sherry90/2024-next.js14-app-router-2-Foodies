e286c922977fa12925bd70027c20802bd0747db9
Sherry90 <hhlsdkm@gmail.com>
2024-05-18 19:19:43 +0900

    feat: 데이터 페칭을 외부 함수Meals로 이관, Suspense를 이용하여 로딩 상태 관리

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,17 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/root/meals";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
-  
+
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -14,14 +19,17 @@ const MealsPage = async () => {
           Delicious meals, created{" "}
           <span className={classes.highlight}>by you</span>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
         <p className={classes.cta}>
           <Link href={"/meals/share"}>Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main className={classes.main}>
-        <MealsGrid meals={meals} />
-      </main>
+      <Suspense fallback={<p className={classes.loading}>Loading...</p>}>
+        <Meals />
+      </Suspense>
+      <main className={classes.main}></main>
     </>
   );
 };
