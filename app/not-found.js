c565e6ed191d7b08a7640e3a459f92b7c2c62325
Sherry90 <hhlsdkm@gmail.com>
2024-05-18 20:41:16 +0900

    fix: text

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,8 @@
 const NotFound = () => {
   return (
     <main className="not-found">
-      <h1>Not Found</h1>
-      <p>Unfortunate, we could not find the requested page or resource.</p>
+      <h1>Meal not Found</h1>
+      <p>Unfortunate, we could not find the requested page or meal data.</p>
     </main>
   );
 };
