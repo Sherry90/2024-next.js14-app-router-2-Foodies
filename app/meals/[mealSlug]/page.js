8c558c956ea4c6f33910c2035953bca529230ac9
Sherry90 <hhlsdkm@gmail.com>
2024-05-18 20:36:59 +0900

    feat: MealDetailsPage

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,37 @@
-const MealDetailsPage = () => {
-  return <h1>Meals Details</h1>;
+import Image from "next/image";
+
+import { getMeal } from "@/root/meals";
+import classes from "./page.module.css";
+import Link from "next/link";
+
+const MealDetailsPage = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <Link href={`mailto:${meal.creator_email}`}>{meal.creator}</Link>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailsPage;
