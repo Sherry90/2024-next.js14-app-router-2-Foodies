e26b50e5fcbaaebe0d3e420e8f7c7d5e8ef50e17
Sherry90 <hhlsdkm@gmail.com>
2024-05-20 09:19:43 +0900

    feat: input을 직접 상호작용하는대신 숨기고 ref로 바인딩, button으로 클릭

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,35 @@
+"use client";
+
+import { useRef } from "react";
+
 import classes from "./image-picker.module.css";
 
 const ImagePicker = ({ label, name }) => {
+  const imageInput = useRef(null);
+  
+  const handlePickClick = () => {
+    imageInput.current.click();
+  };
+  
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
         <input
+          className={classes.input}
           type={"file"}
           id={name}
           accept={"image/png, image/jpeg, image/gif"}
           name={name}
+          ref={imageInput}
         />
+        <button
+          className={classes.button}
+          type={"button"}
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
       </div>
     </div>
   );
