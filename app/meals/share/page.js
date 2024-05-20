14403d0e158cc82db2fd051c8aa87209f84ffafb
Sherry90 <hhlsdkm@gmail.com>
2024-05-20 21:55:40 +0900

    feat: server action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,19 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 const ShareMealPage = () => {
+  const shareMeal = async (formData) => {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      subtitle: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+  };
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +24,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +52,7 @@ const ShareMealPage = () => {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label={"Your image"} name={"image"} />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
