a3b02cbd1bf681a2b7d984d04c8357395141b61d
Sherry90 <hhlsdkm@gmail.com>
2024-05-20 22:03:01 +0900

    feat: server action function

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,20 +1,10 @@
+"use client";
+
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 
 const ShareMealPage = () => {
-  const shareMeal = async (formData) => {
-    "use server";
-
-    const meal = {
-      title: formData.get("title"),
-      subtitle: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-  };
-
   return (
     <>
       <header className={classes.header}>
