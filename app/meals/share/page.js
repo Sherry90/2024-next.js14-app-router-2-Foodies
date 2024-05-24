ae6c20908ffb30efa25853c52f02b64f4b4f0460
Sherry90 <hhlsdkm@gmail.com>
2024-05-24 23:56:29 +0900

    feat: replace button

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 const ShareMealPage = () => {
   return (
@@ -42,7 +43,7 @@ const ShareMealPage = () => {
           </p>
           <ImagePicker label={"Your image"} name={"image"} />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
