45a9bf7d2244591a31a4f34ae8bde48abab6b414
Sherry90 <hhlsdkm@gmail.com>
2024-05-24 23:56:05 +0900

    feat: client side component

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,3 +1,13 @@
-const MealsFormSubmit = () => {}
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+const MealsFormSubmit = () => {
+  const { pending } = useFormStatus();
+  
+  return <button disabled={pending} type="submit">
+    {pending ? "Submitting..." : "Share Meal"}
+  </button>
+}
 
 export default MealsFormSubmit;
